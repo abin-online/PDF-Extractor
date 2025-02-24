ff82874e60f31dcb4e91ba16aa1eb2ad3fdeb6c5
Abin Babu <abinbabuonline@gmail.com>
2025-02-25 01:29:22 +0530

    vite config

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // 👈 Ensure it binds to all network interfaces
+    port:  3000, // 👈 Use Render's port
+  },
+});
